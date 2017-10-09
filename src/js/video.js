20340eb6d2e22b6e41f3fd470806d6f21254ed2c
Andrey Kashta <andrey@hola.org>
2017-10-09 15:58:01 +0300

    export dom utilities for use in external plugins

--- src/js/video.js
+++ src/js/video.js
@@ -729,6 +729,12 @@ videojs.appendContent = Dom.appendContent;
  */
 videojs.insertContent = Dom.insertContent;
 
+/**
+ * Export the Dom utilities for use in external plugins
+ * and Tech's
+ */
+videojs.dom = Dom;
+
 /*
  * Custom Universal Module Definition (UMD)
  *
